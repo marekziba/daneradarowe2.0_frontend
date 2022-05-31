6ae24da2ef3a89102e79d33f3984643d227ce1b1
marekziba <marek.zieba@icloud.com>
2022-05-31 14:11:51 +0200

    Modified models to match database schema

--- src/app/models/Radar.model.ts
+++ src/app/models/Radar.model.ts
@@ -1,29 +1,41 @@
 import { ElementRef } from "@angular/core";
-import { Exclude } from "class-transformer";
+import { Exclude, Expose, Transform, Type } from "class-transformer";
 import { BoundingBox } from "./BoundingBox.model";
 import { Image } from "./Image.model";
 import { Location } from "./Location.model";
 import { Product } from "./Product.model";
+import { ProductVariant } from "./ProductVariant.model";
 import { Scan } from "./Scan.model";
 
 export class Radar {
     id: string;
+
+    @Expose()
     fullName: string;
+
+    @Expose()
     codeName: string;
-    lat: number;
-    lon: number;
-    isDoppler: boolean;
-    isDP: boolean;
-    images: Image[];
-    compositeImages: Image[];
-    scans: Scan[];
-    products: Product[];
+
+    @Expose()
+    @Transform((_) => new Location(_.obj.lat, _.obj.lon))
+    location: Location;
+
+    @Expose()
+    isComposite: boolean;
+
+    @Expose({ groups: ['full'] })
+    @Type(() => ProductVariant)
+    productVariants: ProductVariant[]
+
+    @Expose({ groups: ['full', 'with-products'] })
+    @Type(() => Product)
+    products: Product[]
     
     boundingBox(): BoundingBox {
         return undefined;
     }
 
-    location(): Location {
-        return new Location(this.lat, this.lon)
-    }
+    // location(): Location {
+    //     return new Location(this.lat, this.lon)
+    // }
 }
