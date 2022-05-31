be65517be075101df76ddfabb0950a3fe7ddc643
marekziba <marek.zieba@icloud.com>
2022-05-31 13:50:57 +0200

    Added bounding box to ProductVariant model

--- src/app/models/ProductVariant.model.ts
+++ src/app/models/ProductVariant.model.ts
@@ -1,4 +1,6 @@
-import { Expose } from "class-transformer";
+import { Expose, Transform } from "class-transformer";
+import { BoundingBox } from "./BoundingBox.model";
+import { Location } from "./Location.model";
 
 export class ProductVariant {
     @Expose()
@@ -9,6 +11,10 @@ export class ProductVariant {
 
     @Expose()
     propertyUnit: string;
+
+    @Expose({ toClassOnly: true })
+    @Transform((_) => new BoundingBox(new Location(_.obj.lat_ul, _.obj.lon_ul), new Location(_.obj.lat_lr, _.obj.lon_lr)))
+    boundingBox: BoundingBox;
 
     public static compare(pv1: ProductVariant, pv2: ProductVariant){
         if(pv1.propertyValue > pv2.propertyValue){
